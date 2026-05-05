73c85a59d1e1fd195a6e2d2508bf9f87f1aa94b8
jonaboy19 <jonatantnyudi@gmail.com>
2026-05-05 03:43:03 +0200

    Add mobile nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,10 @@
+import MobileNav from '@/components/MobileNav'
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="bg-black text-white">
+      <body className="bg-black text-white pb-16">
         {children}
+        <MobileNav />
       </body>
     </html>
   );
