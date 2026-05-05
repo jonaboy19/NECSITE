670d42637b5b07cab56499a906db415116008e9b
jonaboy19 <jonatantnyudi@gmail.com>
2026-05-05 03:48:04 +0200

    Add report score to match cards

--- components/MatchCard.tsx
+++ components/MatchCard.tsx
@@ -1,4 +1,5 @@
 import MatchActions from './MatchActions'
+import ReportScore from './ReportScore'
 export default function MatchCard({m}:{m:any}){
  return(
   <div className='kaf-card p-3 rounded-xl flex flex-col gap-1'>
@@ -6,6 +7,7 @@ export default function MatchCard({m}:{m:any}){
    <div className='text-xs text-slate-400'>vs</div>
    <div className='text-sm'>{m.player_b_username||m.player_b_id}</div>
    <div className='text-xs mt-1'>{m.score_a}:{m.score_b}</div>
+   <ReportScore id={m.id}/>
    <MatchActions id={m.id}/>
   </div>
  )
